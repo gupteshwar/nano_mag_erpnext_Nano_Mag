224354acc270d3cf42710b753b735db5a3e4a52e
Sukrut Joshi <sukrut25j@gmail.com>
2020-09-01 20:02:09 +0530

    [FIX][Conditions]

--- erpnext/buying/doctype/purchase_order/purchase_order.py
+++ erpnext/buying/doctype/purchase_order/purchase_order.py
@@ -315,12 +315,16 @@ class PurchaseOrder(BuyingController):
 		
 		'''
 		# frappe.msgprint(print_data)
+
 		if print_data:
 			max_page_html = print_data[0]
 			page_html = print_data[1]
+			# print("-------------",print_data[0],print_data[1])
 			max_page_soup = BeautifulSoup(max_page_html, 'lxml')
 			page_soup = BeautifulSoup(page_html, 'lxml')
+			# print("*********",max_page_soup.text,page_soup.text)
 			if 	max_page_soup.text and page_soup.text:
+				# print("in if")
 				max_page = int(max_page_soup.text)
 				page = int(page_soup.text)
 				if page == max_page:
@@ -328,6 +332,7 @@ class PurchaseOrder(BuyingController):
 				else:
 					return False
 			else:
+				# print("in else")
 				return False
 def item_last_purchase_rate(name, conversion_rate, item_code, conversion_factor= 1.0):
 	"""get last purchase rate for an item"""
